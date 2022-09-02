e008b1c8dc3a0fd852982a3f3fd94730e4a0ea56
compsciAC <68939058+compsciAC@users.noreply.github.com>
2022-09-02 19:09:30 +0100

    Update SmartLamp.java

--- SmartLamp.java
+++ SmartLamp.java
@@ -1,1 +1,23 @@
+package javaSmartHome;
 
+public class SmartLamp extends SmartDevice{
+    private LightModes lightMode; 
+    
+    public SmartLamp(String name, double location, boolean switchedOn, LightModes lightMode){
+       super(name, location, switchedOn);
+       this.lightMode = lightMode;
+    }
+    
+    public void lightMode(LightModes mode){this.lightMode = mode;}
+    public LightModes lightMode(){return lightMode;}
+    
+    public String toString(){
+
+       String check = "";
+
+       check += super.toString();
+       check += "Mode:  " + lightMode + "\n";
+       return check; 
+   }
+    
+}
