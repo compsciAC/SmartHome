4795a02bce3780b3062f092bb32962564f0f505a
compsciAC <68939058+compsciAC@users.noreply.github.com>
2022-09-02 19:07:48 +0100

    Update SmartFridge.java

--- SmartFridge.java
+++ SmartFridge.java
@@ -1,1 +1,39 @@
+package javaSmartHome;
+
+public class SmartFridge extends SmartDevice{
+    private double currentTemprature;
+    
+    public SmartFridge(String name, double location, double currentTemprature){
+        super(name, location, true);
+        this.currentTemprature = currentTemprature;
+    }
+    
+    public void currentTemprature (double value){ 
+        this.currentTemprature = value;
+    }
+    public double currentTemprature(){ 
+        return currentTemprature;
+    }
+    
+    public void increment(){
+        currentTemprature++;
+    }
+    
+    public void decrement(){
+        currentTemprature--;
+    }
+    
+    @Override
+    public void switchOff(){
+        
+    }
+    
+    public String toString(){
+        
+        String check = "";
+        check += super.toString();
+        check += "SWITCHED ON/OFF:  " + currentTemprature + "\n";
+        return check; 
+    }
+}
 
